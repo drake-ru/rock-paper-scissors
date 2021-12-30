c6e98a18dbc6a180244df298d6d9f254fc168d37
Ruby Drake <creative.rdrake@gmail.com>
2021-12-30 12:28:28 +0000

    tidied comments

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 //write function called computerPlay() that randomly returns rock, paper or scissors. This will be the computer's turn played.
-    /* write an array that is ["rock", "paper", "scissors"]. Use Math.floor((Math.random()*10)+3 to get a random result from the array. */
+    /* write an array that is ["rock", "paper", "scissors"]. Use Math.floor((Math.random()*10)+3) to get a random result from the array
+    To make it reproducable, use Math.floor((Math.random()*10)+array.length) so it can always pick from the length of the array. */
 
 const rpsarray = ["rock", "paper", "scissors"];
 function computerPlay(rpsarr) {
    return rpsarr[Math.floor(Math.random() * 3)];
 }
-
-//woop woop!!! This works!!! WAHOO!//
 
 /*Math.floor() = rounds down
 Math.random() = random number between 0 and 1. Therefore will always be 0 when Math.floor(Math.random()
@@ -16,25 +15,11 @@ Math.floor(Math.random()*10)) = random number between 0 and 10 (ie up to and inc
 
 
 
-/*ok, so function that has two paramenters. computerSelection will run the computerPlay function. playerSelection will run a prompt 
-to get you to enter (type) rock, paper or scissors; case insensitive.
-Add if statements to dictate winner -
-if rock and paper
-if rock and scissors
-if paper and scissors
-return a string that says you win / you lose.*/
-
-
-
-
 /*write a function () that plays a single round of rock, paper, scissors.
 The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round and why.
     **make the player's input case insensitive, so they can put rock, ROCk, ROCK etc.
     make sure to return the results of the function call, not console.log() them. But do test out the function using console.log().*/
 
-    /*let playerSelection = window.prompt("Please enter 'rock', 'paper' or 'scissors'.");
-    playerSelection = playerSelection.toLowerCase();
-    let computerSelection = computerPlay(rpsarray);*/
 
     function playsOneRound(playerSelection, computerSelection) {
         
@@ -62,18 +47,16 @@ The function should take two parameters - the playerSelection and computerSelect
 /*Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
 You have not officially learned how to “loop” over code to repeat function calls… 
 if you already know about loops from somewhere else (or if you feel like doing some more learning) feel free to use them. 
-If not, don’t worry! Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
 At this point you should be using console.log() to display the results of each round and the winner at the end.
-Use prompt() to get input from the user.
-Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return value to something more useful.
-Feel free to create more “helper” functions if you think it would be useful. */
+Use prompt() to get input from the user.*/
+
 
 let playerSelection;
     let computerSelection;
     let roundCount = 0;
     let winCount = 0;
+
 function game() {
-    
     for (roundCount = 0; roundCount < 5; ++roundCount) {
         playerSelection = window.prompt("Please enter 'rock', 'paper' or 'scissors'.");
         playerSelection = playerSelection.toLowerCase();
@@ -88,6 +71,9 @@ function game() {
 
 console.log(game());
 
+
+//helper function to be included in game() that checks if the player's hand is a winning one
+
 function didPlayerWin(playerSelection, computerSelection) {
     (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper") ? ++winCount && console.log("You won this round!")
         : (playerSelection === computerSelection) ? console.log("Draw. No points gained.")
@@ -95,11 +81,6 @@ function didPlayerWin(playerSelection, computerSelection) {
 }
 
 
-//game function. Have a variable keeping count of plays of game.
-//Have a variable keeping score of wins.
 
 
 
-/* a helpful function that would distinguish a winning hand...
-function to checkifwinninghand = 
-*/ 
